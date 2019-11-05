60f302e95221b34d219a766054c0181594e16085
Viv^ <vivien.larribau@ipilyon.net>
2019-11-05 09:11:19 +0100

    save 01

---
+++ src/main/java/com/ipiecoles/java/java210/Main.java
@@ -0,0 +1,12 @@
+package com.ipiecoles.java.java210;
+
+import javax.swing.event.TableColumnModelListener;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello jp and clem and Viv^");
+		int entier;
+		entier = 3;
+	}
+}
